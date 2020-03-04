bbcbc7fee692b501526d8d5c32daf8ddd81ed3b7
geyunxiang <geyunxiang_95@sina.com>
2020-03-04 17:20:20 +0800

    init

---
+++ mongodb_database.py
@@ -0,0 +1,91 @@
+"""
+MongoDB is a non-relational database used to store feature values.
+It stores data in JSON format, with a hierarchy of 
+db server -> database -> collection -> record. 
+
+The record looks like this:
+static
+{
+	"scan": "baihanxiang_20190211",
+	"atlas": "brodmann_lrce",
+	"feature": "BOLD.inter.BC",
+	"dynamic": false,
+	"value": "...actual csv str...",
+	"comment": "...descriptive str..."
+}
+
+dynamic
+{
+	"scan": "CMSA_01",
+	"atlas": "brodmann_lrce", 
+	"feature": "BOLD.inter.BC",
+	"dynamic": true, 
+	"window length": 22,
+	"step size": 1, 
+	"value": [
+		"1st str", 
+		"2nd str",
+		"...",
+		"last str"
+	],
+	"comment": "...descriptive str..."
+}
+
+"""
+
+class MongoDBDatabase:
+	"""
+	docstring for MongoDBDatabase
+	"""
+	def __init__(self):
+		pass
+
+	def get_value(self, scanID, atlas_name, feature_name):
+		self.scanID=scanID
+		self.atlats_name=atlas_name
+		self.feature_name=feature_name
+		pass
+
+
+	def set_value(self, scanID, atlas_name, feature_name, value):
+		self.scanID=scanID
+		self.atlats_name=atlas_name
+		self.feature_name=feature_name
+		pass
+
+def generate_database(Directory_name):
+	"""
+	Generate MongoDB from scratch. 
+	Scan a directory and move the directory to MongoDB
+	"""
+	import pymongo
+	client=pymongo.MongoClient(host='localhost',port=27017)
+	db=client['CSV-data']
+	filename=Directory_name
+	pass
+
+def test_MongoDB_query():
+	"""
+	A test program.
+	"""
+	pass
+
+def old_loading_method():
+	"""
+	Scan directory and find target. Uses mmdps.proc.loader
+	"""
+	from mmdps.proc import loader, atlas
+	import pickle
+	atlasobj = atlas.get('brodmann_lrce')
+	net = loader.load_single_network(atlasobj, 'baihanxiang_20190211')
+	print(net.data.shape)
+	print(net.name)
+	data_str = pickle.dumps(net.data)
+
+	attr = loader.load_attrs(['baihanxiang_20190211'], atlasobj, 'BOLD.BC')
+	print(attr[0].data.shape)
+	print(attr[0].name)
+	data_str = pickle.dumps(net.data)
+
+if __name__ == '__main__':
+	old_loading_method()
---
+++ redis_database.py
@@ -0,0 +1,89 @@
+"""
+Redis is a high-speed high-performance cache database.
+A Redis database would be created on-the-fly and (possibly)
+destroyed after usage.
+"""
+
+import redis #这句话好像放不进类里面诶
+import pymongo
+class RedisDatabase:
+	"""
+	docstring for RedisDatabase
+	"""
+
+	#使用了三个变量好麻烦，有没有简化的方法？
+	#我的设计是初始化的时候就连接上monggodb，因为不像redis在运行过程中monggodb不会关闭，所以一开始就连接上就行,不知道对不对？
+	def __init__(self, client = "mongodb://localhost:27017/", db = "runoobdb", col = "static", password=""):
+		self.rdb = redis.Redis()
+		# pool = redis.ConnectionPool()
+		self.mdb_client = pymongo.MongoClient(client)
+		self.mdb = self.mdb_client[db]
+		self.mdb_col = self.mdb[col]
+
+	def is_redis_running(self):
+		return True
+		#会用命令行的话就会有系统问题
+
+	def start_redis(self, host = '127.0.0.1', port = 6379, db=3, password=''):
+		if not self.is_redis_running():
+			pass
+		try:
+			#pool = redis.ConnectionPool(host,port)
+			#self.r = redis.Redis(pool,db)
+			self.rdb = redis.Redis(host, port, db)
+			print('redis coonnection succeded')
+		except Exception as e:
+			print('redis connection failed，error message %s' % e)
+		pass
+
+	def get_data_from_mongodb(self, query = {'scan':'baihanxiang_20190307'}):
+		doc = self.mdb_col.find(query)
+		for i in doc:
+			newkey = i['scan'] + ':' + i['atlas'] + ':' + i['feature'] + ':0'
+			self.rdb.set(newkey, (i['content']))
+		print("The keys have been successfully inserted into redis")
+
+	def generate_key(self, subject_name = 'baihanxiang', scan_date = '20190307', atlas_name = 'aal', feature_name = 'bold_interBC', is_dynamic = False):
+		key = subject_name+'_'+scan_date + ':' + atlas_name + ':' + feature_name + ':0'
+		return key
+
+	def set_value(self, key, value):
+		self.rdb.set(key,value)
+		print('The key has been successfully inserted into redis')
+
+	def get_value(self, key):
+		res = self.rdb.get(key)
+		if res:
+			return res.decode()
+			#return pickle.loads(res.decode())
+		else:
+			list = key.split(':')
+			query = {
+				"scan": list[0],
+				"atlas": list[1],
+				"feature": list[2],
+				"dynamic": 0
+			}
+			self.get_data_from_mongodb(query)
+		#一个一个查询的话，效率会很低，如果设计成批量查询的会更好
+		#同时，由于两个数据库的键值不是一种存储模式，有没有更好的解决的这个问题的方法
+
+def test_generate_Redis():
+	"""
+	A test program that generates Redis database (possibly) based on directory.
+	Note this function will not be used in the released version, since Redis will 
+	query MongoDB to generate database rather than query directory. 
+	"""
+	pass
+
+def test_Redis_query():
+	"""
+	A test program.
+	"""
+	pass
+
+if __name__ == '__main__':
+	a=RedisDatabase()
+	a.start_redis()
+	a.get_data_from_mongodb()
+	print(a.get_value(a.generate_key()))
