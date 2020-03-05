52849e0ef35bd2435ab71c99e1910124dddc5449
姚季涵 <61811791+yaojh18@users.noreply.github.com>
2020-03-05 13:00:57 +0800

    Add files via upload

--- redis_database.py
+++ redis_database.py
@@ -6,27 +6,37 @@ destroyed after usage.
 
 import redis
 import pymongo
+import os
+
 class RedisDatabase:
 	"""
 	docstring for RedisDatabase
 	"""
 
-	#使用了三个变量好麻烦，有没有简化的方法？
-	#我的设计是初始化的时候就连接上monggodb，因为不像redis在运行过程中monggodb不会关闭，所以一开始就连接上就行,不知道对不对？
 	def __init__(self, client = "mongodb://localhost:27017/", db = "runoobdb", col = "static", password=""):
 		self.rdb = redis.Redis()
 		# pool = redis.ConnectionPool()
+		#链接这里会有改动
 		self.mdb_client = pymongo.MongoClient(client)
 		self.mdb = self.mdb_client[db]
 		self.mdb_col = self.mdb[col]
 
+	#我在考虑下面三个代码的实现有没有必要
 	def is_redis_running(self):
-		return True
-		#会用命令行的话就会有系统问题
+		try:
+			process = len(os.popen('tasklist | findstr ' + "redis-server.exe").readlines())
+			print('redis state : %s' % process)
+			if process >= 1:
+				return True
+			else:
+				return False
+		except Exception as e:
+			print('redis connection failed，error message %s' % e)
+			return False
 
-	def start_redis(self, host = '127.0.0.1', port = 6379, db=3, password=''):
+	def start_redis(self, host = '127.0.0.1', port = 6379, db=1, password=''):
 		if not self.is_redis_running():
-			pass
+			os.system("e:/redis/redis-server --service-start")
 		try:
 			#pool = redis.ConnectionPool(host,port)
 			#self.r = redis.Redis(pool,db)
@@ -34,28 +44,48 @@ class RedisDatabase:
 			print('redis coonnection succeded')
 		except Exception as e:
 			print('redis connection failed，error message %s' % e)
-		pass
 
-	def get_data_from_mongodb(self, query = {'scan':'baihanxiang_20190307'}):
+	def stop_redis(self):
+		if self.is_redis_running():
+			self.rdb.flushall()
+			os.system("e:/redis/redis-server --service-stop")
+		print("redis has been stopped")
+
+	#这个接口可能会用monggodb的接口
+	def get_data_from_mongodb(self, query = {'scan':'caipinron_20180412'}):
 		doc = self.mdb_col.find(query)
-		for i in doc:
-			newkey = i['scan'] + ':' + i['atlas'] + ':' + i['feature'] + ':0'
-			self.rdb.set(newkey, (i['content']))
-		print("The keys have been successfully inserted into redis")
+		if doc != None:
+			for i in doc:
+				newkey = i['scan'] + ':' + i['atlas'] + ':' + i['feature'] + ':0'
+				self.rdb.set(newkey, (i['content']))
+			print("The keys have been successfully inserted into redis")
+		else:
+			print("Can't find the key you look for")
 
 	def generate_key(self, subject_name = 'baihanxiang', scan_date = '20190307', atlas_name = 'aal', feature_name = 'bold_interBC', is_dynamic = False):
-		key = subject_name+'_'+scan_date + ':' + atlas_name + ':' + feature_name + ':0'
+		if is_dynamic == False:
+			key = subject_name+'_'+scan_date + ':' + atlas_name + ':' + feature_name + ':0'
+		else:
+			pass #这里写动态数据
 		return key
 
-	def set_value(self, key, value):
+	#重载了赋值接口(这好像不是重载）
+	def set_value_byname(self,subject_name, scan_date, atlas_name, feature_name, is_dynamic, value):
+		key=self.generate_key(subject_name, scan_date, atlas_name, feature_name, is_dynamic)
+		self.set_value_bykey(key,value)
+	def set_value_bykey(self, key, value):
 		self.rdb.set(key,value)
 		print('The key has been successfully inserted into redis')
 
-	def get_value(self, key):
+	#重载了查询接口，查询接口在MongoDB那里面
+	def get_value_byname(self,subject_name, scan_date, atlas_name, feature_name, is_dynamic):
+		key=self.generate_key(subject_name,scan_date,atlas_name,feature_name,is_dynamic)
+		self.get_value_bykey(key)
+	def get_value_bykey(self, key):
 		res = self.rdb.get(key)
 		if res:
 			return res.decode()
-			#return pickle.loads(res.decode())
+			#return pickle.loads(res.decode()) #使用pickle储存的返回情况
 		else:
 			list = key.split(':')
 			query = {
@@ -65,8 +95,8 @@ class RedisDatabase:
 				"dynamic": 0
 			}
 			self.get_data_from_mongodb(query)
-		#一个一个查询的话，效率会很低，如果设计成批量查询的会更好
-		#同时，由于两个数据库的键值不是一种存储模式，有没有更好的解决的这个问题的方法
+	def flushall(self):
+		self.rdb.flushall()
 
 def test_generate_Redis():
 	"""
@@ -85,5 +115,4 @@ def test_Redis_query():
 if __name__ == '__main__':
 	a=RedisDatabase()
 	a.start_redis()
-	a.get_data_from_mongodb()
-	print(a.get_value(a.generate_key()))
+	a.flushall()
