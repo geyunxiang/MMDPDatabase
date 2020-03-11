3d5762871422773828ab596f317c3db951214311
yaojh18 <yaojh18@mails.tsinghua.edu.cn>
2020-03-11 20:44:12 +0800

    这是我写的mongo类

--- mongodb_database.py
+++ mongodb_database.py
@@ -2,7 +2,6 @@
 MongoDB is a non-relational database used to store feature values.
 It stores data in JSON format, with a hierarchy of 
 db server -> database -> collection -> record. 
-
 The record looks like this:
 static
 {
@@ -13,7 +12,6 @@ static
 	"value": "...actual csv str...",
 	"comment": "...descriptive str..."
 }
-
 dynamic
 {
 	"scan": "CMSA_01",
@@ -30,35 +28,58 @@ dynamic
 	],
 	"comment": "...descriptive str..."
 }
-
 """
+import pymongo
 
 class MongoDBDatabase:
 	"""
 	docstring for MongoDBDatabase
 	"""
-	def __init__(self):
-		pass
+	def __init__(self,host, port, m_db, m_col):
+		self.client = pymongo.MongoClient(host, port)
+		self.db = self.client[m_db]
+		self.col = self.db[m_col]
 
 	def get_value(self, scanID, atlas_name, feature_name):
+		self.scanID=scanID
+		self.atlats_name=atlas_name
+		self.feature_name=feature_name
 		pass
 
 	def set_value(self, scanID, atlas_name, feature_name, value):
+		self.scanID=scanID
+		self.atlats_name=atlas_name
+		self.feature_name=feature_name
 		pass
+	def generate_static_query(self,subject_name, scan_date , atlas_name , feature_name):
+		m_query={}
+		if subject_name != '' and scan_date != '':
+			m_query["scan"] = subject_name + '_' + scan_date
+		if atlas_name != '':
+			m_query["atlas"] = atlas_name
+		if feature_name != '':
+			m_query["feature"] = feature_name
+		m_query["dynamic"] = 0
+		return m_query
+	def query_static(self,subject_name, scan_date , atlas_name , feature_name):
+		m_query = self.generate_static_query(subject_name, scan_date , atlas_name , feature_name)
+		return self.col.find(m_query)
+	def exists_static(self,subject_name, scan_date, atlas_name , feature_name):
+		return self.col.count_documents(self.generate_static_query(subject_name, scan_date, atlas_name , feature_name))
 
-def generate_database():
+def generate_database(Directory_name):
+	pass
+def generate_database(host, port, m_db, m_col):
 	"""
 	Generate MongoDB from scratch. 
 	Scan a directory and move the directory to MongoDB
 	"""
-	pass
 
 def test_MongoDB_query():
 	"""
 	A test program.
 	"""
 	pass
-
 def old_loading_method():
 	"""
 	Scan directory and find target. Uses mmdps.proc.loader
@@ -70,12 +91,9 @@ def old_loading_method():
 	print(net.data.shape)
 	print(net.name)
 	data_str = pickle.dumps(net.data)
-
 	attr = loader.load_attrs(['baihanxiang_20190211'], atlasobj, 'BOLD.BC')
-	print(attr[0].data.shape)
+	print(attr[0].data.shape[1])
 	print(attr[0].name)
 	data_str = pickle.dumps(net.data)
-
 if __name__ == '__main__':
 	old_loading_method()
-
