c7b29d1f5587741435c364f6382bd1a1039dd357
handsomewl <wl.tsinghua.ee@gmail.com@github.com>
2020-03-17 22:44:50 +0800

    change mongodb class

--- mongodb_database.py
+++ mongodb_database.py
@@ -39,18 +39,14 @@ import pickle
 from mmdps.proc import loader,atlas
 from mmdps.util import loadsave
 from mmdps import rootconfig
-#print("hello world")
+
 
 root_folder=rootconfig.path.feature_root
-#print(sorted(os.listdir(root_folder)))
-#namelist=['baihanxiang','caipinrong']
 #mriscans=loader.generate_mriscans(namelist)
 atlas_list = ['brodmann_lr', 'brodmann_lrce', 'aal', 'aicha', 'bnatlas']
 attr_list=['BOLD.BC','BOLD.CCFS','BOLD.LE','BLD.WD','BOLD.net','DWI.FA','DWI.MD','DWI.net']
-#print(mriscans)
 atlasobj = atlas.get('brodmann_lrce')
 net = loader.load_single_network(atlasobj, 'baihanxiang_20190211')
-#BOLD.net->bold_net.csv  
 data_str = pickle.dumps(net.data)
 attr = loader.load_attrs(['baihanxiang_20190211'], atlasobj, 'BOLD.BC')
 #l=loader.AttrLoader(atlasobj,root_folder)
@@ -62,59 +58,56 @@ class MongoDBDatabase:
 	"""
 	docstring for MongoDBDatabase
 	"""
+    
+	def __init__(self,host='localhost',port=27017,db="TotalData",col="features",password=''):
+		self.client=pymongo.MongoClient(host,port)
+		self.db=self.client[db]
+		self.collection=self.db[col]
 
-	def __init__(self,NameList):
-		self.NameList=NameList
-		self.client=pymongo.MongoClient('localhost',27017)
-		self.db=self.client['TotalData-database']
-		self.collection=self.db['csvdata-collection']
+	def generate_static_query(self,subject_scan,atlas_name,feature_name):
+		m_query={}
+		if subject_scan!='':
+			m_query["scan"]=subject_scan
+		if atlas_name!='':
+			m_query["atlas"]=atlas_name
+		if feature_name!='':
+			m_query["feature"]=feature_name
+		m_query["dynamic"]="false"
+		m_query["comment"]=""
+		return m_query
+	
+	def query_static(self,subject_scan,atlas_name,feature_name):
+		m_query=self.generate_static_query(subject_scan,atlas_name,feature_name)
+		return self.collection.find(m_query)
 
-	def generate_database(self): 
+
+
+def generate_static_database(): 
 		"""
 		Generate MongoDB from scratch. 
 		Scan a directory and move the directory to MongoDB
 		"""
-		mriscans=loader.generate_mriscans(self.NameList)
-		for mriscan in mriscans:
-			for atlas_name in atlas_list:
-				atlasobj=atlas.get(atlas_name)
-				for attr_name in attr_list:
-					attr=loader.load_attrs(mriscan, atlasobj, attr_name)
-					data_str=pickle.dumps(attr[0].data)	
-					document={
-						"scan":mriscan,
-						"atlas":atlas_name,
-						"feature":attr_name,
-						"value":data_str,
-						"dynamic":"false",
-						"comment":""
-						}
-					self.collection.insert_one(document)
+	database=MongoDBDatabase()
+	for mriscan in mriscans:
+		for atlas_name in atlas_list:				
+			atlasobj=atlas.get(atlas_name)
+			for attr_name in attr_list:
+				attr=loader.load_attrs(mriscan, atlasobj, attr_name)
+				data_str=pickle.dumps(attr[0].data)	
+				document={
+					"scan":mriscan,
+					"atlas":atlas_name,
+					"feature":attr_name,					
+					"value":data_str,
+					"dynamic":"false",
+					"comment":""
+					}
+					database.collection.insert_one(document)
+	return database
+	
 
-	def generate_static_query(self,subject_name, scan_date , atlas_name , feature_name):
-		m_query={}
-		if subject_name != '' and scan_date != '':
-			m_query["scan"] = subject_name + '_' + scan_date
-		if atlas_name != '':
-			m_query["atlas"] = atlas_name
-		if feature_name != '':
-			m_query["feature"] = feature_name
-		m_query["dynamic"] = 0
-		return m_query
 
-	def query_static(self,subject_name, scan_date , atlas_name , feature_name):
-		m_query = self.generate_static_query(subject_name, scan_date , atlas_name , feature_name)
-		return self.collection.find(m_query)
 
-	def exists_static(self,subject_name, scan_date, atlas_name , feature_name):
-		return self.collection.count_documents(self.generate_static_query(subject_name, scan_date, atlas_name , feature_name))
+if __name__ == '__main__':
+	a=generate_static_database()
 
-	"""
-	def old_loading_method():
-	from mmdps.proc import loader, atlas
-	import pickle
-	atlasobj = atlas.get('brodmann_lrce')
-	net = loader.load_single_network(atlasobj, 'baihanxiang_20190211')
-	attr=loader.load_attrs(['baihanxiang_20190211'], atlasobj, 'BOLD.BC')
-	data_str = pickle.dumps(net.data)
-	"""
