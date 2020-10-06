8446e74d09ee80eb18b0cdcb4252674eb4f26237
handsomewl <wl.tsinghua.ee@gmail.com@github.com>
2020-10-06 12:37:27 +0800

    update mongo test script

--- Mongotest.py
+++ Mongotest.py
@@ -1,3 +1,6 @@
+"""
+MongoDB test script goes here.
+"""
 import os
 import time
 import pickle
@@ -25,12 +28,13 @@ DynamiConf = [[22, 1], [50, 1], [100, 1], [100, 3]]
 def generate_static_database_attrs(data_source='Changgung'):
     mdb = MDB.MongoDBDatabase(data_source)
     mriscans = os.listdir(rootconfig.path.feature_root)
-    for mriscan in mriscans:
-        for atlas_name in atlas_list:
-            atlasobj = atlas.get(atlas_name)
-            for attr_name in attr_list:
-                if attr_name.find('net') != -1:
-                    continue
+
+    for atlas_name in atlas_list:
+        atlasobj = atlas.get(atlas_name)
+        for attr_name in attr_list:
+            if attr_name.find('net') != -1:
+                continue
+            for mriscan in mriscans:
                 try:
                     attr = loader.load_attrs([mriscan], atlasobj, attr_name)
                     mdb.save_static_attr(attr[0])
@@ -41,6 +45,7 @@ def generate_static_database_attrs(data_source='Changgung'):
                     print(
                         '! Multiple record found ! scan: %s, atlas: %s, attr: %s' % (
                             mriscan, atlas_name, attr_name))
+            mdb.createIndex('SA', atlas_name+':'+attr_name, ['scan'])
 
 
 def generate_static_database_networks(data_source='Changgung'):
@@ -50,9 +55,9 @@ def generate_static_database_networks(data_source='Changgung'):
     """
     mdb = MDB.MongoDBDatabase(data_source)
     mriscans = os.listdir(rootconfig.path.feature_root)
-    for mriscan in mriscans:
-        for atlas_name in atlas_list:
-            atlasobj = atlas.get(atlas_name)
+    for atlas_name in atlas_list:
+        atlasobj = atlas.get(atlas_name)
+        for mriscan in mriscans:
             try:
                 net = loader.load_single_network(mriscan, atlasobj)
                 mdb.save_static_net(net)
@@ -63,45 +68,55 @@ def generate_static_database_networks(data_source='Changgung'):
                 print(
                     '! Multiple record found ! scan: %s, atlas: %s ' % (
                         mriscan, atlas_name))
+        mdb.createIndex('SN', atlas_name+':'+'BOLD.net', ['scan'])
 
 
 def generate_dynamic_database_attrs(dynamic_rootfolder, data_source='Changgung'):
-    database = MDB.MongoDBDatabase(data_source)
+    mdb = MDB.MongoDBDatabase(data_source)
     mriscans = os.listdir(dynamic_rootfolder)
-    for mriscan in mriscans:
-        for atlas_name in DynamicAtlas:
-            atlasobj = atlas.get(atlas_name)
-            for dynamic_conf in DynamiConf:
-                for attrname in dynamic_attr_list:
+
+    for atlas_name in DynamicAtlas:
+        atlasobj = atlas.get(atlas_name)
+        for dynamic_conf in DynamiConf:
+            for attrname in dynamic_attr_list:
+
+                for mriscan in mriscans:
                     try:
                         attr = loader.load_single_dynamic_attr(
                             mriscan, atlasobj, attrname, dynamic_conf, dynamic_rootfolder)
-                        database.save_dynamic_attr(attr)
+                        mdb.save_dynamic_attr(attr)
                     except OSError:
                         print('! Not found! scan: %s, atlas: %s, attr: %s' %
                               (mriscan, atlas_name, attrname))
                     except MDB.MultipleRecordException:
                         print('! Mutiple found scan: %s,atlas: %s, attr: %s' %
                               (mriscan, atlas_name, attrname))
+                col = mdb.getcol(atlas_name, attrname,
+                                 dynamic_conf[0], dynamic_conf[1])
+                mdb.createIndex('DA', col, ['scan', 'slice'])
 
 
 def generate_dynamic_database_networks(dynamic_rootfolder, data_source='Changgung'):
-    database = MDB.MongoDBDatabase(data_source)
+    mdb = MDB.MongoDBDatabase(data_source)
     mriscans = os.listdir(dynamic_rootfolder)
     atlas_name = 'brodmann_lrce'
     atlasobj = atlas.get(atlas_name)
-    for mriscan in mriscans:
-        for dynamic_conf in DynamiConf:
+
+    for dynamic_conf in DynamiConf:
+        for mriscan in mriscans:
             try:
                 net = loader.load_single_dynamic_network(
                     mriscan, atlasobj, dynamic_conf, dynamic_rootfolder)
-                database.save_dynamic_net(net)
+                mdb.save_dynamic_net(net)
             except OSError:
                 print('! Not found! scan: %s atlas: %s' %
                       (mriscan, atlas_name))
             except MDB.MultipleRecordException:
                 print('! Multiple found! scan: %s atlas:%s' %
                       (mriscan, atlas_name))
+        col = mdb.getcol(atlas_name, 'BOLD.net',
+                         dynamic_conf[0], dynamic_conf[1])
+        mdb.createIndex('DN', col, ['scan', 'slice'])
 
 
 """
